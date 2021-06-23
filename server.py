d2a9ef19f83156d44d6d318c8690b4c069854796
Ian D'Ambrosio <18gdambrosio.stem@gmail.com>
2021-06-22 21:33:51 -0400

    ff

--- server.py
+++ server.py
@@ -2,14 +2,17 @@ import os
 import http.server
 import socketserver
 
+from urlparse import urlparse, parse_qs
 from http import HTTPStatus
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        msg = query_components["msg"] 
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
+        msg = 'Hello! you sent %s' % (msg)
         self.wfile.write(msg.encode())
 
 
